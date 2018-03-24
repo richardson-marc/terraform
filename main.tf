4756b66c57eecde81fb2888aec4a53fa6c07ace5
richardson-marc <marcr@blarg.net>
2018-03-24 15:27:27 -0400

    took out keys

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 # Configure the AWS Provider
 provider "aws" {
     version = "~> 1.9"
-          access_key = "AKIAJ3RQ4BAQX7OLFTKQ"
-	                   secret_key = "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"
+          access_key = "access key"
+	                   secret_key = "secret key"
 			                 profile = "logikcull-test"
 					     region  = "us-east-1"
 					     }
